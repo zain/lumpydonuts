188be75476e511f77d1795aa89477eb533856183
Zain Memon <zain@inzain.net>
2010-10-24 16:22:18 -0700

    Storing meetups to simplegeo

--- meetups.py
+++ meetups.py
@@ -5,21 +5,20 @@ import httplib2, simplegeo, simplejson, urllib
 
 app = Flask(__name__)
 
-
 # meetup settings
 MEETUP_API_KEY = '786e7745f52527d5f5b14783949'
 
 # simplegeo settings
-SIMPLEGEO_AUTH_TOKEN = ''
-SIMPLEGEO_AUTH_SECRET = ''
+SIMPLEGEO_AUTH_TOKEN = 'eTRVDwr6Urg9bFJAKnsvLepwS8Tb7a4V'
+SIMPLEGEO_AUTH_SECRET = 'ePggRKVfYxUCrMdQZgbQKqUXR4zaDGeP'
+SIMPLEGEO_LAYER_NAME = 'meetups'
 
 geo = simplegeo.Client(SIMPLEGEO_AUTH_TOKEN, SIMPLEGEO_AUTH_SECRET)
 
 # THIS IS SPARTA
-SF_TOP_LEFT = Point(-122.5300, 37.8049)
-SF_BOTTOM_RIGHT = Point(-122.3815, 37.7224)
 SF_CENTROID = Point(-122.45575, 37.76365)
 SF_RADIUS = D(mi=5)
+
 
 @app.route("/")
 def index():
@@ -29,10 +28,25 @@ def index():
 def update():
     # make the meetup call
     meetups = fetch_meetups(SF_CENTROID, SF_RADIUS)
-    
     app.logger.info("Fetched a total of %s meetups using the API." % len(meetups))
     
+    # eliminate all the meetups that don't have a public venue (so no lat/lon)
+    meetups = filter(lambda m: 'venue' in m, meetups)
+    
+    # add topics to the group section of each meetup
+    meetups = add_topics(meetups)
+    
     # store in simplegeo
+    records = [meetup2record(m) for m in meetups]
+    app.logger.debug("%s records to add to simplegeo." % len(records))
+    
+    i = 0
+    while i < len(records):
+        app.logger.debug("Adding #%s to #%s." % (i, i+99))
+        geo.add_records(SIMPLEGEO_LAYER_NAME, records[i:i+99])
+        i += 99
+    
+    return "Done!"
 
 def fetch_meetups(point, radius):
     url = "http://api.meetup.com/2/open_events.json?%s" % urllib.urlencode({
@@ -46,16 +60,35 @@ def fetch_meetups(point, radius):
     
     meetups, meta = meetup_api_call(url)
     
-    total_count = meta['total_count']
+    app.logger.info("%s/%s meetups fetched." % (len(meetups), meta['total_count']))
     
-    app.logger.info("%s/%s meetups fetched." % (len(meetups), total_count))
-    
-    while 'next' in meta and meta['next']:
+    while False and 'next' in meta and meta['next']:
         new_meetups, meta = meetup_api_call(meta['next'])
         meetups += new_meetups
         
-        app.logger.info("%s/%s meetups fetched (+%s)." % (len(meetups), total_count, 
+        app.logger.info("%s/%s meetups fetched (+%s)." % (len(meetups), meta['total_count'], 
             len(new_meetups)))
+    
+    return meetups
+
+def add_topics(meetups):
+    app.logger.debug("Fetching groups for %s meetups." % len(meetups))
+    
+    uniqueify = lambda l: list(set(l))
+    
+    url = "https://api.meetup.com/groups.json?%s" % urllib.urlencode({
+        'key': MEETUP_API_KEY,
+        'id': ",".join(uniqueify([str(m['group']['id']) for m in meetups])),
+    })
+    
+    groups, meta = meetup_api_call(url)
+    
+    # change into a table of {'id': ['topic1', 'topic2', 'topic3']}
+    group_topics = dict([(g['id'], [t['name'] for t in g['topics']]) for g in groups])
+    
+    for meetup in meetups:
+        group_id = str(meetup['group']['id'])
+        meetup['group']['topics'] = group_topics[group_id]
     
     return meetups
 
@@ -67,18 +100,18 @@ def meetup_api_call(url):
     
     if resp['status'] != '200':
         app.logger.error("API call returned: %s" % content)
-        return []
+        return [], {}
     
-    try:
-        response = simplejson.loads(unicode(content, 'latin1'), strict=False)
-    except simplejson.JSONDecodeError, e:
-        app.logger.error("JSONDecodeError: %s" % e)
-        import ipdb; ipdb.set_trace()
+    response = simplejson.loads(unicode(content, 'latin1'), strict=False)
     
-    meetups = response['results']
-    meta = response['meta']
-    
-    return meetups, meta
+    return response['results'], response['meta']
+
+def meetup2record(meetup):
+    return simplegeo.Record(SIMPLEGEO_LAYER_NAME, meetup['id'], meetup['venue']['lat'],
+        meetup['venue']['lon'], event_url=meetup['event_url'], group_name=meetup['group']['name'],
+        topics=":".join(meetup['group']['topics']), name=meetup['name'], time=meetup['time'],
+        trending_rank=meetup['trending_rank'], yes_rsvp_count=meetup['yes_rsvp_count'],
+        venue_name=meetup['venue']['name'], venue_id=meetup['venue']['id'])
 
 
 if __name__ == "__main__":
